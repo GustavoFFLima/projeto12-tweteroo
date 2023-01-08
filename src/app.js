7381e98e0120cad140208cadbfa3617058bd04a1
GustavoFFLima <frazao39@gmail.com>
2023-01-07 21:33:38 -0300

    memoria global

--- src/app.js
+++ src/app.js
@@ -3,5 +3,15 @@ import express from 'express'
 const app = express()
 const PORT = 5000
 
+let usuarios = {
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}
+
+let tweet = {
+	username: "bobesponja",
+  tweet: "eu amo o hub"
+} 
+
 
 app.listen(PORT, () => console.log(`esta rodando belezinha na porta : ${PORT}`))
