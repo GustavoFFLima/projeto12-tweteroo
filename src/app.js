c69955488ed5e48e0d2808ea6df4ee06133cd51b
GustavoFFLima <frazao39@gmail.com>
2023-01-08 13:26:56 -0300

    IMPLEMENTANDO TWEETS

--- src/app.js
+++ src/app.js
@@ -1,17 +1,46 @@
 import express from 'express'
+import cors from "cors"
 
 const app = express()
 const PORT = 5000
 
-let usuarios = {
-	username: 'bobesponja', 
-	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-}
+const usuarios = []
 
-let tweet = {
-	username: "bobesponja",
-  tweet: "eu amo o hub"
-} 
+const tweet = []
 
+app.use(express.json())
+app.use(cors())
 
 app.listen(PORT, () => console.log(`esta rodando belezinha na porta : ${PORT}`))
+
+app.post("/sign-up", (req, res) =>{
+    const dados = req.body
+    const id = usuarios.length + 1;
+
+    dados.id = id;
+    usuarios.push(dados)
+
+    res.send("ok")
+})
+
+app.post("/tweets", (req, res) =>{
+  const dados = req.body
+  const id = tweet.length + 1;
+
+  dados.id = id;
+  if(!usuarios.find(dados.username)) {
+    return `UNAUTHORIZED`
+  }
+  tweet.push(dados)
+
+  res.send("ok")
+})
+
+
+// app.get("/tweets", (req, res) =>{
+//     //salvar os dados do usuario
+//     res,send('ok')
+// })
+
+
+
